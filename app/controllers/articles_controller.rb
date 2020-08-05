cf049eb515460821e5a9cf5cc265126a13875f79
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-08-05 15:54:29 -0400

    Complete article validations and flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      # success
       flash[:success] = 'Article has been created'
       redirect_to articles_path
     else
-      # error
-      flash[:danger] = 'Article was not created'
-      redirect_to 'new'
+      flash.now[:danger] = 'Article has not been created'
+      render :new
     end
   end
 
